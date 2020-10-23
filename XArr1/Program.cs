9ce507dacd79930738c7a80afbaa045bbd6b3330
Tomas Kindahl <tomas.kindahl@molk.se>
2020-10-23 14:07:04 +0200

    Added XArr1 results

--- XArr1/Program.cs
+++ XArr1/Program.cs
@@ -4,21 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace XArr2
+namespace XArr1
 {
     class Program
     {
         static void Main(string[] args)
         {
             Random R = new Random();
-            int[] rval = new int[10];
-            for (int i = 0; i < 10; i++)
+            for(int i = 0; i < 10; i++)
             {
-                rval[i] = R.Next(1, 11);
-            }
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("{0}: {1}", i, rval[i]);
+                Console.WriteLine("{0}", R.Next(1, 11));
             }
         }
     }
